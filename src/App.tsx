52761b39be34493c468ea9898492e500acb98432
Rila Goto <lesalisite@gmail.com>
2021-06-13 22:03:49 +0900

    App.tsxでダミーデータ一覧を表示できるようにする

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,47 @@
 import React from "react";
 import "./App.css";
 import { BookToRead } from "./BookToRead";
+import BookRow from "./BookRow";
 
 const dummyBooks: BookToRead[] = [
   {
     id: 1,
     title: "はじめてのReact",
     authors: "ダミー",
-    memo: ""
+    memo: "",
   },
   {
     id: 2,
     title: "React Hooks入門",
     authors: "ダミー",
-    memo: ""
+    memo: "",
   },
   {
     id: 3,
     title: "実践Reactアプリケーション開発",
     authors: "ダミー",
-    memo: ""
-  }
+    memo: "",
+  },
 ];
 
 const App = () => {
+  const bookrows = dummyBooks.map((b) => {
+    return (
+      <BookRow
+        book={b}
+        key={b.id}
+        onMemoChange={(id, memo) => {}}
+        onDelete={(id) => {}}
+      />
+    );
+  });
   return (
     <div className="App">
       <section className="nav">
         <h1>読みたい本リスト</h1>
         <div className="button-like">本を追加</div>
       </section>
-      <section className="main">
-        <h1>チュートリアルを始めましょう</h1>
-      </section>
+      <section className="main">{bookrows}</section>
     </div>
   );
 };
